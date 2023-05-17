ab54598456c333a38c51f59ff9e55f4e9057b8f1
hayoenn2 <dwd1205@naver.com>
2023-05-17 17:13:08 +0900

    [Style] 기본 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="game">
+      <div className="game-board">game-board</div>
+      <div className="game-info">game-info</div>
+    </div>
+  );
 }
 
 export default App;
