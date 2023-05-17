fec571902628f94c06bff023028a25f130d16354
hayoenn2 <dwd1205@naver.com>
2023-05-18 00:37:32 +0900

    [Add] class를 이용해 state 추가하기

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,15 +2,22 @@ import React from "react";
 import "./Square.css";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
       <button
         className="square"
         onClick={() => {
-          console.log("click!");
+          this.setState({ value: "X" });
         }}
       >
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
