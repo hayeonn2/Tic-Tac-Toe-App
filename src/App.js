d6309d761ab2d97d12181b9524a325d326e28b7a
hayoenn2 <dwd1205@naver.com>
2023-05-22 09:30:27 +0900

    [Fix] 앱 스타일링 변경

--- src/App.js
+++ src/App.js
@@ -64,7 +64,9 @@ function App() {
 
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{desc}</button>
+        <button className="move-button" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
       </li>
     );
   });
@@ -82,7 +84,7 @@ function App() {
       </div>
       <div className="game-info">
         <div className="status">{status}</div>
-        <ol>{moves}</ol>
+        <ol style={{ listStyle: "none" }}>{moves}</ol>
       </div>
     </div>
   );
